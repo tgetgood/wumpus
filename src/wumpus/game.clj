69208bdf9f5ca99952833a682f6380e3e5c044d9
Oskar M. Grande <oskar@keatext.ai>
2019-06-17 19:24:51 -0400

    Add starting point

--- src/wumpus/game.clj
+++ src/wumpus/game.clj
@@ -7,3 +7,16 @@
    :edge-num 45
    :worm-gangs 3
    :cop-odds 1/15})
+
+(defn random-edge [n]
+  (let [a (rand-int n)
+        b (rand-int n)]
+    (if (= a b)
+      (random-edge n)
+      [a b])))
+
+(defn create-world [parameters]
+  (let [areas (range 0 (parameters :node-num))
+        roads (into #{}) 
+                    (map (fn [_] (random-edge (parameters :node-num))) 
+                    (range 0 (parameters :edge-num)))])))

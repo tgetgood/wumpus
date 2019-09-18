6975e6aad4c967116e29bd1c34607de222a0ade1
Thomas Getgood <thomas.getgood@gmail.com>
2019-09-18 17:07:49 -0400

    Implemented the first part of the game from the book.

--- src/wumpus/game.clj
+++ src/wumpus/game.clj
@@ -1,4 +1,5 @@
-(ns wumpus.game)
+(ns wumpus.game
+  (:require [clojure.set :as set]))
 
 (def parameters
   "Static parameters that determine the game world.
@@ -8,42 +9,112 @@
    :worm-gangs 3
    :cop-odds 1/15})
 
-(defn random-edge [n]
-  (let [a (rand-int n)
-        b (rand-int n)]
-    (if (= a b)
-      (random-edge n)
-      [a b])))
+;;;;; Graph creation
 
-(defn edge-graph
-  "Create a hash map of the edges, e.g. {1 #{4 3 2}, 2 #{3}, 3 #{4}}"
-  [edges]
-  (into {}
-        (map (fn [[k v]]
-               [k (into #{} (map second v))])
-             (group-by first edges)))
+(defn random-node
+  "Returns a random node from nodes"
+  [nodes]
+  (rand-nth nodes))
 
-(defn depth-first-search
-  "Create a list of list of edges by islands, e.g. [[[1 2] [2 3]] [[5 6] [6 7]]]"
-  [nodes edges]
-  (let [graph (group-by first edges)]
-    (loop [visited #{}
-           components []]
-      (if (= nodes visited)
-        components
-        ()))))
+(defn edge-pair
+  "Given a pair of nodes, return a pair of edges between them, one in either
+  direction."
+  [a b]
+  (when-not (= a b)
+    #{a b}))
 
-(defn connect-graph [areas roads]
-  (let [connected-areas (depth-first-search areas roads)
-        root-nodes (map (comp first first) connected-areas)
-        extra-edges (map vector root-nodes (drop 1 root-nodes))]
-    extra-edges
-    ))
+(defn make-edge-list
+  "Returns a random list of num (pairs of) edges connecting random nodes."
+  [nodes num-edges]
+  (->> (repeatedly (partial random-node (into [] nodes)))
+       (partition 2)
+       (map (partial apply edge-pair))
+       (remove nil?)
+       distinct
+       (take num-edges)))
 
-(defn create-world [params]
-  (let [n (:node-num params)
-        e (:edge-num params)
-        areas (into #{} (range n))
-        roads (into #{} (take e (repeatedly #(random-edge n))))
-        ]
-    ))
+;;;;; Make sure the city is connected
+
+(defn direct-edges
+  "Returns edges incident on the given node."
+  [node edge-list]
+  (filter #(contains? % node) edge-list))
+
+(defn traverse
+  "Helper for get-connected."
+  [node visited edge-list]
+  (let [visited (conj visited node)]
+    (into visited
+          (comp
+           (map second)
+           (remove #(contains? visited %))
+           (mapcat #(traverse % visited edge-list)))
+          (direct-edges node edge-list))))
+
+(defn get-connected
+  "Given a node. returns the set of all nodes reachable from that node in the
+  graph."
+  [node edge-list]
+  (traverse node #{} edge-list))
+
+(defn find-islands
+  "Given a graph in the form of a set of nodes and a list of edges, return all
+  connected subsets of the node set."
+  [nodes edge-list]
+  (let [connected (get-connected (first nodes) edge-list)
+        unconnected (set/difference nodes connected)]
+    (if (empty? unconnected)
+      #{connected}
+      (conj (find-islands unconnected edge-list) connected))))
+
+(defn connect-with-bridges
+  "Given a set of islands, return a list of edges which when added to the graph
+  will make it fully connected."
+  [[i1 i2 & more]]
+  (when i2
+    (conj (connect-with-bridges (conj more i2))
+            (edge-pair (first i1) (first i2)))))
+
+(defn connect-all-islands
+  "Given a set of nodes and a list of edges, returns a new list of edges which
+  make a graph with no islands"
+  [nodes edge-list]
+  (concat (connect-with-bridges (find-islands nodes edge-list)) edge-list))
+
+;;;;; Generate a city
+
+
+(defn edgelist->map [edges]
+  (merge (group-by first edges)
+         (group-by second edges)))
+
+(defn maybe-add-cops [odds x]
+  (if (< (rand) odds)
+    (with-meta x {:cops true})
+    x))
+
+(defn make-city-edges [parameters]
+  (let [nodes (into #{} (map inc (range (:node-num parameters))))
+        edge-list (connect-all-islands
+                   nodes (make-edge-list nodes (:edge-num parameters)))]
+    (map (partial maybe-add-cops (:cop-odds parameters)) edge-list)))
+
+(defn within-1? [x y])
+
+(defn within-2? [x y])
+
+(defn make-city-nodes [places edges parameters]
+  (let [wumpus     (random-node places)
+        edge-map   (edgelist->map edges)
+        glow-worms (->> (repeatedly (partial random-node places))
+                        (take (:worm-gangs parameters))
+                        (into #{}))]
+
+    (map (fn [node]
+           (cond-> {:name node}
+             (= node wumpus)                              (assoc :wumpus true)
+             (within-2? node wumus)                       (assoc :blood true)
+             (contains? glow-worms node)                  (assoc :glow-worm true)
+             (some #(within-1? node %) glow-worms)        (assoc :lights true)
+             (some #(:cops (meta x)) (get edge-map node)) (assoc :sirens true)))
+         nodes)))
